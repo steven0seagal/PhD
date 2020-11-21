fff00200ca8105d3d305b9a66da7b58bbc64a1e2
Bartek <bartosz.piotr.baranowski@gmail.com>
2020-11-21 10:06:19 +0100

    url account correct

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login', views.login, name = 'login'),
-    path('register', views.register, name = 'register'),
+    # path('register', views.register, name = 'register'),
     path('logout', views.logout, name = 'logout'),
     path('dashboard', views.dashboard, name = 'dashboard'),
     path('dashboard/neighborhood',views.dashboard_neigh, name = 'dash_neigh'),
